ea92a9566d69ca73c693a22683a334c168978585
Thad Stinson <bootytee@Thads-iMac.local>
2019-06-15 13:50:59 -0500

    gemfile, cors.rb, routes.rb, and model done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/workouts', to: 'workouts#index'
+  get '/workouts/:id', to: 'workouts#show'
+  post '/workouts', to: 'workouts#create'
+  delete '/workouts/:id', to: 'workouts#delete'
+  put '/workouts/:id', to: 'workouts#update'
 end
